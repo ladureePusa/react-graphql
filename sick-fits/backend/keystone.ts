412bebdcfd0d4f906d70b6550db2ff1ca4cc90e6
ladureePusa <joanna.jiang@rea-group.com>
2021-05-28 14:04:23 +1000

    step 12 - setting up keystone and typescript

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,30 @@
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import "dotenv/config";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, //how long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  // @ts-ignore
+  server: {
+    cos: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databaseURL,
+  },
+  lists: createSchema({
+    //schema items go in there
+  }),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+});
